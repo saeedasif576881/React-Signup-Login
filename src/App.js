1f2a0ac996faa76fb5b28003c389e1439ad50efd
Saeed-Asif <saeed03095263441@gmail.com>
2024-11-13 01:22:25 +0500

    FIRST PORJECT OF REACT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import "./style.css";
 
 function App() {
+  const [count, setcount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btn" onClick={() => setcount(count + 1)}>Count</button>
+      <div>{count}</div>
     </div>
   );
 }
